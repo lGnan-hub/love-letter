7b007334838ceb2c2e312d8a95324282dcede61e
lGnan-hub <gnan.lopinti@gmail.com>
2024-04-21 20:46:35 +0530

    SpeedAnalytics introduced

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LoveLetter from './components/LoveLetter/LoveLetter';
 import './App.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <div className="App">
       <LoveLetter />
+      <SpeedInsights/>
     </div>
   );
 }
